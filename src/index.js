a31abcde9106f1f2cd82b90bf4f7371f9cb0f03f
Thomas Dashney <tdashney@gmail.com>
2017-10-01 22:32:18 -0400

    export createAction

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as mount } from './mount'
 export { default as createReducer } from './create-reducer'
+export { default as createAction } from './create-action'
--- test/index.test.js
+++ test/index.test.js
@@ -5,4 +5,5 @@ import * as reduxModular from '../src/index'
 it('exports modularize and createReducer', () => {
   expect(reduxModular).toHaveProperty('mount')
   expect(reduxModular).toHaveProperty('createReducer')
+  expect(reduxModular).toHaveProperty('createAction')
 })
